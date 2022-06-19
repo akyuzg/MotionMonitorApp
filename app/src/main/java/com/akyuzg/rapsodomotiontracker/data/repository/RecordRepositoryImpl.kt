857767907e4f8f3ad0f72199ea30babb7bbb1b22
Güray Akyüz <gur.akyuz@gmail.com>
2022-06-19 16:51:47 +0300

    create repository for records

---
+++ app/src/main/java/com/akyuzg/rapsodomotiontracker/data/repository/RecordRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package com.akyuzg.rapsodomotiontracker.data.repository
+
+import com.akyuzg.rapsodomotiontracker.data.local.RecordDao
+import com.akyuzg.rapsodomotiontracker.domain.model.Position
+import com.akyuzg.rapsodomotiontracker.domain.model.Record
+import com.akyuzg.rapsodomotiontracker.domain.repository.RecordRepository
+import kotlinx.coroutines.flow.Flow
+
+class RecordRepositoryImpl(
+    private val dao: RecordDao
+): RecordRepository {
+
+    override suspend fun getRecords(): Flow<List<Record>> {
+        return dao.getRecords()
+    }
+
+    override suspend fun getPositions(recordId: Int): Flow<List<Position>> {
+        return dao.getPositions(recordId)
+    }
+
+    override suspend fun createRecord(record: Record) {
+        return dao.createRecord(record)
+    }
+
+    override suspend fun insertPosition(position: Position) {
+        return dao.insertPosition(position)
+    }
+
+}
---
+++ app/src/main/java/com/akyuzg/rapsodomotiontracker/domain/repository/RecordRepository.kt
@@ -0,0 +1,16 @@
+package com.akyuzg.rapsodomotiontracker.domain.repository
+
+import com.akyuzg.rapsodomotiontracker.domain.model.Position
+import com.akyuzg.rapsodomotiontracker.domain.model.Record
+import kotlinx.coroutines.flow.Flow
+
+interface RecordRepository {
+
+    suspend fun getRecords(): Flow<List<Record>>
+
+    suspend fun getPositions(recordId: Int): Flow<List<Position>>
+
+    suspend fun createRecord(record: Record)
+
+    suspend fun insertPosition(position: Position)
+}
